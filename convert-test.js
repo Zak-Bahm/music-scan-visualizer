5f94cd90b81ea24c3edca29217d47681417f2430
Zacharias Bahm <zakbahm@gmail.com>
2022-04-26 01:40:26 -0400

    new test data

--- convert-test.js
+++ convert-test.js
@@ -6,16 +6,36 @@ for (const key in testData) {
     if (Object.hasOwnProperty.call(testData, key)) {
         const dp = testData[key];
 
+        // {
+        //     "tsne_1": 13.2785921097,
+        //     "tsne_2": -28.0624332428,
+        //     "genre_id": 0,
+        //     "track_id": 71089,
+        //     "genre_name": "Electronic",
+        //     "track_title": "Stealth Elk",
+        //     "artist_name": "Bitbasic",
+        //     "mean_1": -7.949991703,
+        //     "mean_2": -22.8180713654,
+        //     "std_1": 22.0991428567,
+        //     "std_2": 23.6134650244,
+        //     "z_1": 0.9606066818,
+        //     "z_2": -0.2220920086
+        // }
+
         const converted = {
             location: {
                 x: dp.tsne_1,
-                y: dp.tsne_2
+                y: dp.tsne_2,
+                xDev: dp.z_1,
+                yDev: dp.z_2
             },
             genre: {
-                id: parseInt(dp.label)
+                id: parseInt(dp.genre_id),
+                name: dp.genre_name
             },
             info: {
-                id: parseInt(dp.track)
+                artist: dp.artist_name,
+                title: dp.track_title
             }
         };
 
