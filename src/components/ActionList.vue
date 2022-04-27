80dd6b838730f563509771290eff0f1ee9c63502
Zacharias Bahm <zakbahm@gmail.com>
2022-04-27 14:30:25 -0400

    add update mechanism

--- src/components/ActionList.vue
+++ src/components/ActionList.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="btn-list d-flex flex-column">
+        <Transition>
+            <div class="backdrop my-2 d-flex justify-content-between align-items-center" v-if="updateExists">
+                <h5 class="m-0">Update Available: </h5>
+                <button
+                    class="btn btn-success update-btn ms-2"
+                    @click="refreshApp()"
+                >
+                    <fa-icon icon="cloud-arrow-down" size="1x"/>
+                </button>
+            </div>
+        </Transition>
         <div class="backdrop my-2">
             <h3>Standard Deviations:</h3>
             <div class="btn-group d-flex dev-btns" role="group" aria-label="Standard Deviation Selection">
@@ -44,6 +55,11 @@ const emit = defineEmits(['changeGenre', 'changeDev'])
 const genreColors = inject<string[]>('genreColors');
 const genreNames = inject<string[]>('genreNames');
 
+// setup update refs
+const refreshing = ref(false);
+const registration = ref();
+const updateExists = ref(false);
+
 // setup functions
 function changeGenres(index: number) {
     let newGenre = index;
@@ -53,6 +69,34 @@ function changeGenres(index: number) {
 
     emit('changeGenre', newGenre);
 }
+
+// Listen for our custom event from the SW registration
+document.addEventListener('swUpdated', updateAvailable, { once: true })
+
+// Prevent multiple refreshes
+navigator.serviceWorker.addEventListener('controllerchange', () => {
+    if (refreshing.value) return
+    refreshing.value = true
+    // Here the actual reload of the page occurs
+    window.location.reload()
+});
+
+// Store the SW registration so we can send it a message
+// We use `updateExists` to control whatever alert, toast, dialog, etc we want to use
+// To alert the user there is an update they need to refresh for
+function updateAvailable(event: any) {
+    registration.value = event.detail
+    updateExists.value = true
+}
+
+// Called when the user accepts the update
+function refreshApp() {
+    updateExists.value = false
+    // Make sure we only send a 'skip waiting' message if the SW is waiting
+    if (!registration.value || !registration.value.waiting) return
+    // send message to SW to skip the waiting and activate the new SW
+    registration.value.waiting.postMessage({ type: 'SKIP_WAITING' })
+}
 </script>
 
 <style>
@@ -61,6 +105,16 @@ function changeGenres(index: number) {
     }
     .dev-btns button.selected-dev {
         background-color: rgb(107 119 131);
+    }
+
+    button.update-btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 30px;
+        height: 30px;
+        border-radius: 15px;
+        cursor: pointer;
     }
 
     .genre-option {
--- src/utilities/fontawesome-icons.ts
+++ src/utilities/fontawesome-icons.ts
@@ -1,13 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTrashCan, faExternalLink, faPlay, faPause, faForward, faBackward, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan, faExternalLink, faPlay, faPause, faForward, faBackward, faSpinner, faDownload, faCloudArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
     faTrashCan,
     faExternalLink,
     faPlay, faPause,
     faForward, faBackward,
-    faSpinner
+    faSpinner, faDownload,
+    faCloudArrowDown
 );
 
 export default FontAwesomeIcon;
